9218a18b34bb4b7b40a13ec85755ba640aaa02c2
bitcoinfees <bitcoinfees@gmail.com>
2014-11-12 18:13:15 +0800

    E-step complete, also changed to use shelve instead of pickle

--- model/collectdata/collect.py
+++ model/collectdata/collect.py
@@ -4,6 +4,7 @@ from time import sleep, time
 from txmempool import TxMempool
 from bitcoin.rpc import Proxy
 from bitcoin.core import b2lx
+import shelve
 
 statVersion = '0.1'
 
@@ -24,6 +25,8 @@ if not os.path.exists(datadir):
     os.mkdir(datadir)
 
 proxy = Proxy()
+shelfFile = os.path.join(datadir, 'blockstats_v' + str(statVersion) + '.shelf')
+shelf = shelve.open(shelfFile)
 
 def collect():
 
@@ -33,60 +36,64 @@ def collect():
     prevHeight = proxy.getblockcount()
     mempool.update()
     sleep(pollperiod)
+    try:
+        while True:
+            currHeight = proxy.getblockcount()
+            if currHeight == prevHeight:
+                txDelta, _discard1, _discard2 = mempool.update()
+                if txDelta < 0:
+                    logWrite('Warning, mempool entries removed when no new block was found.', currHeight=currHeight)
+                sleep(pollperiod)
+            else:
+                numNewBlocks = currHeight - prevHeight
+                mempoolStats = {}
+                blockTxList = {}
+                for blockHeight in range(prevHeight+1, currHeight+1):
+                    block = proxy.getblock(proxy.getblockhash(blockHeight))
+                    blockRcvTime = time()
+                    blockTxList[blockHeight] = [tx.GetHash() for tx in block.vtx]
 
-    while True:
-        currHeight = proxy.getblockcount()
-        if currHeight == prevHeight:
-            txDelta, _discard1, _discard2 = mempool.update()
-            if txDelta < 0:
-                logWrite('Warning, mempool entries removed when no new block was found.', currHeight=currHeight)
-            sleep(pollperiod)
-        else:
-            numNewBlocks = currHeight - prevHeight
-            mempoolStats = {}
-            blockTxList = {}
-            for blockHeight in range(prevHeight+1, currHeight+1):
-                block = proxy.getblock(proxy.getblockhash(blockHeight))
-                blockRcvTime = time()
-                blockTxList[blockHeight] = [tx.GetHash() for tx in block.vtx]
+                    mempoolStats[blockHeight] = [{
+                        'txid': txm.txidHex,
+                        'inBlock': txid in blockTxList[blockHeight],
+                        'feeRate': txm.feeRate,
+                        'priority': txm.computePriority(offset=currHeight-blockHeight+1, currHeight=currHeight),
+                        'size': txm.nTxSize,
+                        'dependants': map(b2lx, txm.dependants),
+                        'dependencies': map(b2lx, txm.dependencies),
+                        'timedelta': blockRcvTime-txm.rcvTime
+                    } for txid,txm in mempool.txpool.iteritems()]
 
-                mempoolStats[blockHeight] = [{
-                    'txid': txm.txidHex,
-                    'inBlock': txid in blockTxList[blockHeight],
-                    'feeRate': txm.feeRate,
-                    'priority': txm.computePriority(offset=currHeight-blockHeight+1, currHeight=currHeight),
-                    'size': txm.nTxSize,
-                    'dependants': map(b2lx, txm.dependants),
-                    'dependencies': map(b2lx, txm.dependencies),
-                    'timedelta': blockRcvTime-txm.rcvTime
-                } for txid,txm in mempool.txpool.iteritems()]
+                    for txid in blockTxList[blockHeight]:
+                        mempool.deleteTx(txid)
 
-                for txid in blockTxList[blockHeight]:
-                    mempool.deleteTx(txid)
+                prevHeight = proxy.getblockcount()
+                if prevHeight != currHeight:
+                    logWrite('Blocks are coming too quickly, we skipped one here.', currHeight=prevHeight)
 
-            prevHeight = proxy.getblockcount()
-            if prevHeight != currHeight:
-                logWrite('Blocks are coming too quickly, we skipped one here.', currHeight=currHeight)
+                _discard1, removedSet, _discard2 = mempool.update()
+                removedSet = map(b2lx, removedSet)
+                for stat in mempoolStats.values():
+                    for tx in stat:
+                        if tx['txid'] in removedSet:
+                            # A tx that did not get included in a block, yet got removed from mempool:
+                            # Means that it was invalidated by the latest blocks (double spent), so 
+                            # we don't want to use it in the fee estimations.
+                            stat.remove(tx)
 
-            _discard1, removedSet, _discard2 = mempool.update()
-            removedSet = map(b2lx, removedSet)
-            for stat in mempoolStats.values():
-                for tx in stat:
-                    if tx['txid'] in removedSet:
-                        # A tx that did not get included in a block, yet got removed from mempool:
-                        # Means that it was invalidated by the latest blocks (double spent), so 
-                        # we don't want to use it in the fee estimations.
-                        stat.remove(tx)
+                for blockHeight, stat in mempoolStats.items():
+                    numTxsInBlock = len(blockTxList[blockHeight])-1
+                    numMempoolTxsInBlock = len([1 for tx in stat if tx['inBlock']])
+                    logWrite(str(numMempoolTxsInBlock) + ' of ' + str(numTxsInBlock) + ' in block ' + str(blockHeight))
 
-            for blockHeight, stat in mempoolStats.items():
-                numTxsInBlock = len(blockTxList[blockHeight])-1
-                numMempoolTxsInBlock = len([1 for tx in stat if tx['inBlock']])
-                logWrite(str(numMempoolTxsInBlock) + ' of ' + str(numTxsInBlock) + ' in block ' + str(blockHeight))
+                    shelf[str(blockHeight)] = stat
+                    # with open(os.path.join(datadir, str(blockHeight)+'v'+str(statVersion)+'.pickle'),'wb') as dataFile:
+                    #     pickle.dump(stat, dataFile)
 
-                with open(os.path.join(datadir, str(blockHeight)+'v'+str(statVersion)+'.pickle'),'wb') as dataFile:
-                    pickle.dump(stat, dataFile)
-
-            sleep(pollperiod)
+                sleep(pollperiod)
+    
+    except KeyboardInterrupt:
+        shelf.close()
 
 
 def logWrite(entry, toStdOut=True, currHeight=None):
--- model/em.py
+++ model/em.py
@@ -1,6 +1,10 @@
 from random import randint
 from numpy.random import dirichlet
 from scipy.special import comb, beta
+from config import config
+from os import path
+import cPickle as pickle
+from operator import mul
 
 timeDeltaMargin = 5
 hardMaxBlockSize = 1e6 # The hard-coded block size limit
@@ -10,18 +14,32 @@ numPolicies = 5
 minFeeRateInitRange = (100, 50000)
 maxBlockSizeInitRange = (0, hardMaxBlockSize)
 
+datadir = config['em']['datadir']
+dataformat = config['em']['dataformat']
+
 class Policy:
     def __init__(self, minFeeRate, maxBlockSize, weight):
         self.minFeeRate = minFeeRate
         self.maxBlockSize = maxBlockSize
         self.weight = weight
 
+    def maximize(self, weights, stats):
+        pass
+
+    def __repr__(self):
+        return "Policy(MinFeeRate: %d, MaxBlockSize: %d, Weight: %.3f)" % (self.minFeeRate, self.maxBlockSize, self.weight)
+        # return str(self.minFeeRate)+', ' + str(self.maxBlockSize) + ', ' + str(self.weight)
+
+
 class ModelParams:
     def __init__(self, numPolicies=numPolicies, alpha=defaultAlpha):
         policyWeights = dirichlet([1]*numPolicies)
         self.policies = [Policy(randint(*minFeeRateInitRange),
-            randint(*maxBlockSizeInitRange),policyWeights[i]) for i in range(numPolicies)]
+            hardMaxBlockSize,policyWeights[i]) for i in range(numPolicies)]
         self.alpha = alpha
+
+    # def __repr__(self):
+    #     return self.policies, self.alpha
 
 class BlockStats:
     def __init__(self, blockData):
@@ -38,6 +56,7 @@ class BlockStats:
 
         self.stats = filter(lambda x: x['timedelta'] > mintime, self.stats)
         self.stats.sort(key=lambda x: x['feeRate'], reverse=True)
+        self.blockSize = sum([tx['size'] for tx in self.stats if tx['inBlock']])
 
     def _txCriteria(self, tx):
         '''tx must have > 0 feeRate and all its mempool dependencies must be inBlock.'''
@@ -45,22 +64,89 @@ class BlockStats:
         return tx['feeRate'] and all([dep['inBlock'] for dep in deps])
 
     def calcWeights(self, params):
-        weights = [[0]*len(params.policies) for i in ('fee','size')]
+        weights = [0]*len(params.policies)
         for pidx,policy in enumerate(params.policies):
-            for lidx,limitState in enumerate(('fee', 'size')):
-                likelihood = self.calcLikelihood(policy,limitState,params.alpha)
+            likelihood = self.calcLikelihood(policy,params.alpha)
+            weights[pidx] = likelihood*policy.weight
+        weightSum = sum(weights)
+        if weightSum > 0:
+            self.weights = map(lambda x: x/weightSum, weights)
+        else:
+            self.weights = [0]*len(params.policies)
 
-    def calcLikelihood(self, policy, limitState, alpha):
-        if limitState == 'fee':
-            n = len(self.stats)
-            k = len([1 for tx in stats if tx['feeRate'] >= policy.minFeeRate and tx['inBlock']
+    def calcLikelihood(self, policy, alpha):
+        
+        if self.blockSize > policy.maxBlockSize+blockSizeStdDev:
+            return 0
+        else:
+            k = len([1 for tx in self.stats if tx['feeRate'] >= policy.minFeeRate and tx['inBlock']
                 or tx['feeRate'] < policy.minFeeRate and not tx['inBlock']])
-            return pbb(k,n,alpha)
-        elif limitState == 'size':
-            
+            n = len(self.stats)
+            if self.blockSize > policy.maxBlockSize-blockSizeStdDev:
+                try:
+                    feeThreshold = min([tx['feeRate'] for tx in self.stats 
+                        if tx['feeRate'] > policy.minFeeRate and tx['inBlock']])
+                    
+                    nDiscount = len([1 for tx in self.stats if tx['feeRate'] < feeThreshold
+                        and tx['feeRate'] > policy.minFeeRate])
+                except ValueError:
+                    nDiscount = len([1 for tx in self.stats if tx['feeRate'] > policy.minFeeRate])
 
-def pbb(k,n,alpha,b=1):
+                n -= nDiscount
+
+            return pbb(k,n,alpha)
+
+class EM:
+    def __init__(self):
+        self.params = ModelParams()
+        self.data = []
+    
+    def addBlock(self, blockData):
+        self.data.append(BlockStats(blockData))
+
+    def eStep(self):
+        for block in self.data: 
+            block.calcWeights(self.params)
+
+    def mStep(self):
+        newWeights = [0].len(self.params.policies)
+        for pidx,policy in enumerate(self.params):
+            weights = [block.weights[pidx] for block in self.data]
+            policy.maximize(weights, self.data)
+            newWeights[pidx] = sum(weights)
+        newWeightsSum = sum(newWeights)
+        for pidx,policy in enumerate(self.params):
+            policy.weight = newWeights[pidx]/newWeightsSum
+        # Remember to re-estimate alpha!
+
+def pbb(k,n,alpha):
+    numerator = reduce(mul, range(k+1,k+alpha), 1)
+    denominator = reduce(mul, range(n+1, n+alpha+1), 1)
+    return float(numerator)*alpha/denominator
+
+
+def pbb2(k,n,alpha,b=1):
     return comb(n,k)*beta(k+alpha,n-k+b)/beta(alpha,b)
+
+if __name__ == '__main__':
+
+    blockRange = (329400, 329450)
+    em = EM()
+
+    for height in range(*blockRange):
+        filename = path.join(datadir, str(height) + dataformat)
+        try:
+            with open(filename, 'rb') as f:
+                b = pickle.load(f)
+                # print "Loading block " + str(height) + '...'
+                em.addBlock(b)
+        except IOError:
+            print "Block not found, moving on"
+
+
+
+
+
 
 
 
