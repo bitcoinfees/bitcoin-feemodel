7ee4fd7c93040a114679ba1d3eedeb58af5c3a4b
bitcoinfees <bitcoinfees@gmail.com>
2014-12-31 09:40:20 +0800

    redid wait measurement to use saveable

--- feemodel/measurement.py
+++ feemodel/measurement.py
@@ -11,8 +11,9 @@ except ImportError:
 
 feeResolution = config['queue']['feeResolution']
 priorityThresh = config['measurement']['priorityThresh']
-samplingInterval = 18
-rateInterval = 2016
+samplingWindow = 18
+rateWindow = 2016
+waitTimesWindow = 2016
 
 class BlockTxRate(object):
     def __init__(self, block, prevBlock):
@@ -28,12 +29,13 @@ class BlockTxRate(object):
     def __eq__(self, other):
         return self.__dict__ == other.__dict__
 
+
 class TxRates(Saveable):
-    def __init__(self, samplingInterval=samplingInterval, rateInterval=rateInterval):
+    def __init__(self, samplingWindow=samplingWindow, rateWindow=rateWindow):
         self.blockRates = {}
         self.txSamples = []
-        self.samplingInterval = samplingInterval
-        self.rateInterval = rateInterval
+        self.samplingWindow = samplingWindow
+        self.rateWindow = rateWindow
         self.prevBlock = None
         super(TxRates, self).__init__(saveRatesFile)
 
@@ -44,16 +46,17 @@ class TxRates(Saveable):
                     self.blockRates[block.height] = BlockTxRate(block, self.prevBlock)
                 except ValueError:
                     pass
+                
+                samplingThresh = block.height - self.samplingWindow
+                rateThresh = block.height - self.rateWindow
                 for height, blockRate in self.blockRates.items():
-                    samplingThresh = block.height - self.samplingInterval
-                    rateThresh = block.height - self.rateInterval
                     if height <= samplingThresh:
                         blockRate.txSamples = []
                     if height <= rateThresh:
                         del self.blockRates[height]
 
                 self.txSamples = [] 
-                for height in range(block.height-self.samplingInterval+1, block.height+1):
+                for height in range(block.height-self.samplingWindow+1, block.height+1):
                     blockRate = self.blockRates.get(height)
                     if blockRate:
                         self.txSamples.extend(blockRate.txSamples)
@@ -88,31 +91,78 @@ class TxRates(Saveable):
         return self.__dict__ == other.__dict__
 
 
+class BlockTxWaitTimes(object):
+    def __init__(self, feeClassValues):
+        self.avgWaitTimes = {feeRate: (0, 0.) for feeRate in feeClassValues}
 
-    # def calcRates(self, blockHeightRange):
-    #     self.txSamples = {}
-    #     prevBlock = None
-    #     for height in range(*blockHeightRange):
-    #         block = Block.blockFromHistory(height)
-    #         if block and prevBlock:
-    #             newtxs = set(block.entries) - set(prevBlock.entries)
-    #             self.txSamples.update({
-    #                 txid: block.entries[txid]
-    #                 for txid in newtxs
-    #             })
-    #             for txid, entry in block.entries.iteritems():
-    #                 if entry.get('isConflict') and txid in self.txSamples:
-    #                     del self.txSamples[txid]
-    #             self.totalTime += block.time - prevBlock.time
+    def addTx(self, feeRate, waitTime):
+        feeClass = None
+        for feeClassValue in self.avgWaitTimes:
+            if feeClassValue <= feeRate:
+                feeClass = max(feeClass, feeClassValue)
 
-    #         prevBlock = block
-
-    #     if self.totalTime:
-    #         self.txRate = len(self.txSamples) / float(self.totalTime)
-    #     else:
-            # logWrite("TxRates error: measurement interval is zero.")
+        if feeClass:
+            numTxs, prevWaitTime = self.avgWaitTimes[feeClass]
+            self.avgWaitTimes[feeClass] = (numTxs+1,
+                (prevWaitTime*numTxs + waitTime) / (numTxs + 1))
 
 
+class TxWaitTimes(Saveable):
+    def __init__(self, feeClassValues):
+        self.blockWaitTimes = {}
+        self.feeClassValues = feeClassValues
+        self.waitTimes = {}
+        super(TxWaitTimes, self).__init__(saveWaitFile)
+        
+    def pushBlocks(self, blocks):
+        for block in blocks:
+            if not block:
+                continue
+            self.blockWaitTimes[block.height] = BlockTxWaitTimes(self.feeClassValues)
+            for entry in block.entries.itervalues():
+                if self._countTx(entry):
+                    self.blockWaitTimes[block.height].addTx(
+                        entry['feeRate'], block.time - entry['time'])
+            logWrite("WT: Added block %d" % block.height)
+
+            heightThresh = block.height - waitTimesWindow
+            for height in self.blockWaitTimes.keys():
+                if height <= heightThresh:
+                    del self.blockWaitTimes[height]
+            
+            self.calcWaitTimes()
+
+    def calcWaitTimes(self):
+        if not len(self.blockWaitTimes):
+            raise ValueError("WT: No valid blocks.")
+        self.waitTimes = {}
+        for feeClassValue in self.feeClassValues:
+            totalTxs = sum([wtBlock.avgWaitTimes[feeClassValue][0]
+                for wtBlock in self.blockWaitTimes.itervalues()])
+            if totalTxs:
+                self.waitTimes[feeClassValue] = (
+                    sum
+                    ([
+                        wtBlock.avgWaitTimes[feeClassValue][0]*
+                        wtBlock.avgWaitTimes[feeClassValue][1]
+                        for wtBlock in self.blockWaitTimes.itervalues()
+                    ]) / totalTxs,
+                    totalTxs
+                )
+            else:
+                self.waitTimes[feeClassValue] = (-1, totalTxs)
+
+    @staticmethod
+    def _countTx(entry):
+        return (
+            entry['inBlock'] and
+            not entry['depends'] and
+            entry['currentpriority'] < priorityThresh
+        )
+
+    @staticmethod
+    def loadObject():
+        return super(TxWaitTimes,TxWaitTimes).loadObject(saveWaitFile)
 
 def poissonSample(l):
     # http://en.wikipedia.org/wiki/Poisson_distribution#Generating_Poisson-distributed_random_variables
@@ -125,104 +175,108 @@ def poissonSample(l):
     return k - 1
 
 
-class WaitMeasure(object):
-    def __init__(self, maxMFR, adaptive, loadFile=saveWaitFile):
-        self.adaptive = adaptive
-        self.blockData = {}
-        self.maxMFR = maxMFR
-        self.waitTimes = None
-        self.bestHeight = None
-
-        if loadFile:
-            try:
-                self.loadBlockData()
-            except IOError:
-                logWrite("WM: Couldn't load saved measurements; loading from disk.")
-            else:
-                logWrite("WM: Saved measurements loaded: %s" % (repr(self),))
-
-    def getStats(self):
-        if self.waitTimes:
-            return [repr(self)] + [(idx*feeResolution,) + t
-                for idx,t in enumerate(self.waitTimes)]
-        else:
-            return []
-
-    def pushBlocks(self, blocks, isInit=False):
-        for block in blocks:
-            if not block:
-                continue
-            self.blockData[block.height] = WaitMeasureBlock(self.maxMFR)
-            self.bestHeight = block.height
-            numTxs = 0
-            for entry in block.entries.itervalues():
-                if (entry['inBlock']
-                    and entry['feeRate'] < (self.maxMFR + feeResolution)
-                    and not entry['depends']
-                    and entry['currentpriority'] < priorityThresh):
-                        self.blockData[block.height].addTx(
-                            entry['feeRate'], block.time - entry['time'])
-                        numTxs += 1
-            logWrite("WM: Added block %d with %d transactions" %
-                        (block.height, numTxs))
-        if not isInit:
-            self.adaptiveCalc()
-            try:
-                self.saveBlockData()
-            except IOError:
-                logWrite("WM: Couldn't save block data.")
-
-    def adaptiveCalc(self):
-        if not self.bestHeight:
-            raise ValueError("WM: Empty block data.")
-
-        heightThresh = self.bestHeight - self.adaptive
-        for height in self.blockData.keys():
-            if height < heightThresh:
-                del self.blockData[height]
-
-        self.waitTimes = [None] * (self.maxMFR // feeResolution + 1)
-        for idx in range(self.maxMFR // feeResolution + 1):
-            totalTxs = sum([wmBlock.avgWaitTimes[idx][0]
-                for wmBlock in self.blockData.itervalues()])
-            if totalTxs:
-                self.waitTimes[idx] = (sum([wmBlock.avgWaitTimes[idx][0]*wmBlock.avgWaitTimes[idx][1]
-                    for wmBlock in self.blockData.itervalues()]) / totalTxs, totalTxs)
-            else:
-                self.waitTimes[idx] = (-1, totalTxs)
-
-        # self.waitTimes = [
-        #     sum([wmBlock.avgWaitTimes[idx][0]*wmBlock.avgWaitTimes[idx][1]
-        #         for wmBlock in self.blockData.itervalues()]) /
-        #     sum([wmBlock.avgWaitTimes[idx][0]
-        #         for wmBlock in self.blockData.itervalues()])
-        #     for idx in range(self.maxMFR // feeResolution + 1)
-        # ]
-
-    def saveBlockData(self, dbFile=saveWaitFile):
-        with open(dbFile, 'wb') as f:
-            pickle.dump(self.blockData,f)
-
-    def loadBlockData(self, dbFile=saveWaitFile):
-        with open(dbFile, 'rb') as f:
-            self.blockData = pickle.load(f)
-        try:
-            self.bestHeight = max(self.blockData)
-            self.maxMFR = (len(self.blockData[self.bestHeight].avgWaitTimes)-1)*feeResolution
-        except ValueError:
-            self.bestHeight = None
-
-    def __repr__(self):
-        return "WM{maxMFR: %d, adaptive: %d, bestHeight: %d, numBlocks: %d}" % (
-            self.maxMFR, self.adaptive, self.bestHeight if self.bestHeight else -1, len(self.blockData))
 
 
-class WaitMeasureBlock(object):
-    def __init__(self, maxMFR):
-        self.avgWaitTimes = [(0, 0.)]*(maxMFR // feeResolution + 1)
 
-    def addTx(self, feeRate, waitTime):
-        feeClassIdx = feeRate // feeResolution
-        numTxs, prevWaitTime = self.avgWaitTimes[feeClassIdx]
-        self.avgWaitTimes[feeClassIdx] = (numTxs+1,
-            (prevWaitTime*numTxs + waitTime) / (numTxs + 1))
+
+# class WaitMeasure(object):
+#     def __init__(self, maxMFR, adaptive, loadFile=saveWaitFile):
+#         self.adaptive = adaptive
+#         self.blockData = {}
+#         self.maxMFR = maxMFR
+#         self.waitTimes = None
+#         self.bestHeight = None
+
+#         if loadFile:
+#             try:
+#                 self.loadBlockData()
+#             except IOError:
+#                 logWrite("WM: Couldn't load saved measurements; loading from disk.")
+#             else:
+#                 logWrite("WM: Saved measurements loaded: %s" % (repr(self),))
+
+#     def getStats(self):
+#         if self.waitTimes:
+#             return [repr(self)] + [(idx*feeResolution,) + t
+#                 for idx,t in enumerate(self.waitTimes)]
+#         else:
+#             return []
+
+#     def pushBlocks(self, blocks, isInit=False):
+#         for block in blocks:
+#             if not block:
+#                 continue
+#             self.blockData[block.height] = WaitMeasureBlock(self.maxMFR)
+#             self.bestHeight = block.height
+#             numTxs = 0
+#             for entry in block.entries.itervalues():
+#                 if (entry['inBlock']
+#                     and entry['feeRate'] < (self.maxMFR + feeResolution)
+#                     and not entry['depends']
+#                     and entry['currentpriority'] < priorityThresh):
+#                         self.blockData[block.height].addTx(
+#                             entry['feeRate'], block.time - entry['time'])
+#                         numTxs += 1
+#             logWrite("WM: Added block %d with %d transactions" %
+#                         (block.height, numTxs))
+#         if not isInit:
+#             self.adaptiveCalc()
+#             try:
+#                 self.saveBlockData()
+#             except IOError:
+#                 logWrite("WM: Couldn't save block data.")
+
+#     def adaptiveCalc(self):
+#         if not self.bestHeight:
+#             raise ValueError("WM: Empty block data.")
+
+#         heightThresh = self.bestHeight - self.adaptive
+#         for height in self.blockData.keys():
+#             if height < heightThresh:
+#                 del self.blockData[height]
+
+#         self.waitTimes = [None] * (self.maxMFR // feeResolution + 1)
+#         for idx in range(self.maxMFR // feeResolution + 1):
+#             totalTxs = sum([wmBlock.avgWaitTimes[idx][0]
+#                 for wmBlock in self.blockData.itervalues()])
+#             if totalTxs:
+#                 self.waitTimes[idx] = (sum([wmBlock.avgWaitTimes[idx][0]*wmBlock.avgWaitTimes[idx][1]
+#                     for wmBlock in self.blockData.itervalues()]) / totalTxs, totalTxs)
+#             else:
+#                 self.waitTimes[idx] = (-1, totalTxs)
+
+#         # self.waitTimes = [
+#         #     sum([wmBlock.avgWaitTimes[idx][0]*wmBlock.avgWaitTimes[idx][1]
+#         #         for wmBlock in self.blockData.itervalues()]) /
+#         #     sum([wmBlock.avgWaitTimes[idx][0]
+#         #         for wmBlock in self.blockData.itervalues()])
+#         #     for idx in range(self.maxMFR // feeResolution + 1)
+#         # ]
+
+#     def saveBlockData(self, dbFile=saveWaitFile):
+#         with open(dbFile, 'wb') as f:
+#             pickle.dump(self.blockData,f)
+
+#     def loadBlockData(self, dbFile=saveWaitFile):
+#         with open(dbFile, 'rb') as f:
+#             self.blockData = pickle.load(f)
+#         try:
+#             self.bestHeight = max(self.blockData)
+#             self.maxMFR = (len(self.blockData[self.bestHeight].avgWaitTimes)-1)*feeResolution
+#         except ValueError:
+#             self.bestHeight = None
+
+#     def __repr__(self):
+#         return "WM{maxMFR: %d, adaptive: %d, bestHeight: %d, numBlocks: %d}" % (
+#             self.maxMFR, self.adaptive, self.bestHeight if self.bestHeight else -1, len(self.blockData))
+
+
+# class WaitMeasureBlock(object):
+#     def __init__(self, maxMFR):
+#         self.avgWaitTimes = [(0, 0.)]*(maxMFR // feeResolution + 1)
+
+#     def addTx(self, feeRate, waitTime):
+#         feeClassIdx = feeRate // feeResolution
+#         numTxs, prevWaitTime = self.avgWaitTimes[feeClassIdx]
+#         self.avgWaitTimes[feeClassIdx] = (numTxs+1,
+#             (prevWaitTime*numTxs + waitTime) / (numTxs + 1))
