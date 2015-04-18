497c33a8c858b962b605954ae39a5a9b5726a168
bitcoinfees <bitcoinfees@gmail.com>
2015-04-18 16:15:38 +0800

    Refactor estimate.pools
    Bring pool name ID out into its own function - get_blockname

--- feemodel/estimate/pools.py
+++ feemodel/estimate/pools.py
@@ -126,47 +126,16 @@ class PoolsEstimator(SimPools):
             if stopflag and stopflag.is_set():
                 raise StopIteration("Stop flag set.")
             try:
-                baddrs, btag = get_coinbase_info(height)
+                name = get_blockname(height)
                 blocksize = get_block_size(height)
                 numhashes = get_hashesperblock(height)
             except IndexError:
                 raise IndexError("Bad block range.")
 
-            name = None
-            for paddr, pattrs in self.poolinfo['payout_addresses'].items():
-                candidate_name = pattrs['name']
-                if paddr in baddrs:
-                    if name is None:
-                        name = candidate_name
-                    elif name != candidate_name:
-                        logger.warning("> 1 pools mapped to block %d" % height)
-
-            for ptag, pattrs in self.poolinfo['coinbase_tags'].items():
-                candidate_name = pattrs['name']
-                if ptag in btag:
-                    if name is None:
-                        name = candidate_name
-                    elif name != candidate_name:
-                        logger.warning("> 1 pools mapped to block %d" % height)
-
-            if name is None:
-                for addr in baddrs:
-                    if addr is not None:
-                        # Underscore indicates that the pool is not in the
-                        # list of known pools. We use the first valid
-                        # coinbase addr as the name.
-                        name = addr[:12] + '_'
-                        break
-
-            if name is None:
-                logger.warning(
-                    "Unable to identify pool of block %d." % height)
-                name = 'U' + str(height) + '_'
-
             self.blockmap[height] = (name, blocksize, numhashes)
 
         for height in self.blockmap.keys():
-            if height < blockrangetuple[0] or height >= blockrangetuple[1]:
+            if not blockrangetuple[0] <= height < blockrangetuple[1]:
                 del self.blockmap[height]
 
         if not self.blockmap:
@@ -239,3 +208,41 @@ class PoolsEstimator(SimPools):
         print(tabulate(table, headers=headers))
         print("Avg block interval is %.2f" % (1./self.blockrate,))
         print("Total hashrate is {} Thps.".format(totalhashrate*1e-12))
+
+
+def get_blockname(height):
+    """Assign a name to a block, denoting the entity that mined it.
+
+    Uses blockchain.info's knownpools.json.
+    """
+    baddrs, btag = get_coinbase_info(height)
+    name = None
+    for paddr, pattrs in knownpools['payout_addresses'].items():
+        candidate_name = pattrs['name']
+        if paddr in baddrs:
+            if name is not None and name != candidate_name:
+                logger.warning("> 1 pools mapped to block %d" % height)
+            name = candidate_name
+
+    for ptag, pattrs in knownpools['coinbase_tags'].items():
+        candidate_name = pattrs['name']
+        if ptag in btag:
+            if name is not None and name != candidate_name:
+                logger.warning("> 1 pools mapped to block %d" % height)
+            name = candidate_name
+
+    if name is None:
+        for addr in baddrs:
+            if addr is not None:
+                # Underscore indicates that the pool is not in the
+                # list of known pools. We use the first valid
+                # coinbase addr as the name.
+                name = addr[:12] + '_'
+                break
+
+    if name is None:
+        logger.warning(
+            "Unable to identify pool of block %d." % height)
+        name = 'U' + str(height) + '_'
+
+    return name
